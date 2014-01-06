8452074c5bf72cf9ddc98ef5054e46fc35b2047f
sunhui <sun_wit@126.com>
2014-01-06 15:58:30 +0800

    宝典提交

---
+++ baodian/data/data.js
@@ -0,0 +1,173 @@
+﻿localStorage.clear();
+localStorage.setItem( 'v', '0.6' );
+localStorage.setItem( 'f', '[]' );
+localStorage.setItem( 'h', '[]' );
+
+localStorage.setItem( '1_CSS 简介', 'CSS 概述CSS 指层叠样式表 (Cascading Style Sheets)样式定义如何显示 HTML 元素样式通常存储在样式表中把样式添加到 HTML 4.0 中，是为了解决内容与表现分离的问题样式表可以极大提高工作效率外部样式表通常存储在 CSS 文件中多个样式定义可层叠为一' );
+localStorage.setItem( '1_CSS 基础语法', 'CSS 规则由两个主要的部分构成：选择器，以及一条或多条声明。' );
+localStorage.setItem( '1_CSS 高级语法', '你可以对选择器进行分组，这样，被分组的选择器就可以分享相同的声明。用逗号将需要分组的选择器分开。在下面的例子中，我们对所有的标题元素进行了分组。所有的标题元素都是绿色的。' );
+localStorage.setItem( '1_CSS 派生选择器', '通过依据元素在其位置的上下文关系来定义样式，你可以使标记更加简洁。' );
+localStorage.setItem( '1_CSS id选择器', 'id 选择器可以为标有特定 id 的 HTML 元素指定特定的样式。id 选择器以 "#" 来定义。' );
+localStorage.setItem( '1_CSS 类选择器', '在 CSS 中，类选择器以一个点号显示。' );
+localStorage.setItem( '1_CSS 属性选择器', '对带有指定属性的 HTML 元素设置样式。' );
+localStorage.setItem( '1_CSS 创建', '外部样式表，内部样式表，内联样式，多重样式' );
+localStorage.setItem( '1_CSS 背景', 'CSS 允许应用纯色作为背景，也允许使用背景图像创建相当复杂的效果。' );
+localStorage.setItem( '1_CSS 文本', 'CSS 文本属性可定义文本的外观' );
+localStorage.setItem( '1_CSS 字体', 'CSS 字体属性定义文本的字体系列、大小、加粗、风格（如斜体）和变形（如小型大写字母）。' );
+localStorage.setItem( '1_CSS 列表', 'CSS 列表属性允许你放置、改变列表项标志，或者将图像作为列表项标志。' );
+localStorage.setItem( '1_CSS 表格', 'CSS 表格属性可以帮助您极大地改善表格的外观。' );
+localStorage.setItem( '1_CSS 轮廓', '轮廓（outline）是绘制于元素周围的一条线，位于边框边缘的外围，可起到突出元素的作用。' );
+localStorage.setItem( '1_CSS 框模型简介', 'CSS 框模型 (Box Model) 规定了元素框处理元素内容、内边距、边框 和 外边距 的方式。' );
+localStorage.setItem( '1_CSS 内边距', '元素的内边距在边框和内容区之间。控制该区域最简单的属性是 padding 属性。' );
+
+localStorage.setItem( '2_CSS3 教程', 'CSS 用于控制网页的样式和布局。')
+localStorage.setItem( '2_CSS3 简介', 'CSS3 完全向后兼容，因此您不必改变现有的设计。浏览器通常支持 CSS2。')
+localStorage.setItem( '2_CSS3 边框', '通过 CSS3，您能够创建圆角边框，向矩形添加阴影，使用图片来绘制边框 - 并且不需使用设计软件，比如 PhotoShop。')
+localStorage.setItem( '2_CSS3 背景', 'background-size, background-origin')
+localStorage.setItem( '2_CSS3 文本效果', 'text-shadow, word-wrap')
+localStorage.setItem( '2_CSS3 字体', '@font-face：')
+localStorage.setItem( '2_CSS3 2D 转换', '通过 CSS3 转换，我们能够对元素进行移动、缩放、转动、拉长或拉伸。')
+localStorage.setItem( '2_CSS3 3D 转换', 'CSS3 允许您使用 3D 转换来对元素进行格式化。')
+localStorage.setItem( '2_CSS3 过渡', '通过 CSS3，我们可以在不使用 Flash 动画或 JavaScript 的情况下，当元素从一种样式变换为另一种样式时为元素添加效果。')
+localStorage.setItem( '2_CSS3 动画', '通过 CSS3，我们能够创建动画，这可以在许多网页中取代动画图片、Flash 动画以及 JavaScript。')
+localStorage.setItem( '2_CSS3 多列', '通过 CSS3，您能够创建多个列来对文本进行布局 - 就像报纸那样！')
+localStorage.setItem( '2_CSS3 用户界面', '在 CSS3 中，新的用户界面特性包括重设元素尺寸、盒尺寸以及轮廓等。')
+localStorage.setItem( '2_CSS2 打印参考', '打印 HTML 文档总是会出现问题。在 CSS2 中，我们可以借助打印属性让打印 web 内容更容易一些。')
+localStorage.setItem( '2_CSS2 听觉参考', '听觉样式表可把语音合成与音响效果相组合，使用户可以听到信息，而无需进行阅读。')
+localStorage.setItem( '2_CSS 颜色', '十六进制颜色')
+
+localStorage.setItem( '3_JavaScript 简介', 'JavaScript 是脚本语言,JavaScript 是一种轻量级的编程语言。JavaScript 是可插入 HTML 页面的编程代码。JavaScript 插入 HTML 页面后，可由所有的现代浏览器执行。JavaScript 很容易学习。' );
+localStorage.setItem( '3_JavaScript 使用', 'HTML 中的脚本必须位于 <script> 与 </script> 标签之间' );
+localStorage.setItem( '3_JavaScript 输出', '如需从 JavaScript 访问某个 HTML 元素，您可以使用 document.getElementById(id) 方法。' );
+localStorage.setItem( '3_JavaScript 语句', 'document.getElementById("demo").innerHTML="Hello World";' );
+localStorage.setItem( '3_JavaScript 注释', 'JavaScript 注释可用于提高代码的可读性。' );
+localStorage.setItem( '3_JavaScript 变量', '变量是存储信息的容器。' );
+localStorage.setItem( '3_JavaScript 数据类型', '字符串、数字、布尔、数组、对象、Null、Undefined' );
+localStorage.setItem( '3_JavaScript 对象', 'JavaScript 中的所有事务都是对象：字符串、数字、数组、日期，等等。' );
+localStorage.setItem( '3_JavaScript 函数', '函数是由事件驱动的或者当它被调用时执行的可重复使用的代码块。' );
+localStorage.setItem( '3_JavaScript 运算符', 'JavaScript 算术运算符' );
+localStorage.setItem( '3_JavaScript 比较和逻辑运算符', '比较和逻辑运算符用于测试 true 或 false。' );
+localStorage.setItem( '3_JavaScript If...Else 语句', '条件语句用于基于不同的条件来执行不同的动作。' );
+localStorage.setItem( '3_JavaScript Switch 语句', 'switch 语句用于基于不同的条件来执行不同的动作。' );
+localStorage.setItem( '3_JavaScript For 循环', '循环可以将代码块执行指定的次数。' );
+localStorage.setItem( '3_JavaScript While 循环', '只要指定条件为 true，循环就可以一直执行代码。' );
+localStorage.setItem( '3_JavaScript Break 和 Continue 语句', 'break 语句用于跳出循环。' );
+
+localStorage.setItem( '5_HTML 5 简介', 'HTML5 将成为 HTML、XHTML 以及 HTML DOM 的新标准。' );
+localStorage.setItem( '5_HTML 5 视频', 'HTML5 规定了一种通过 video 元素来包含视频的标准方法。' );
+localStorage.setItem( '5_HTML 5 Video + DOM', 'HTML5 <video> 元素同样拥有方法、属性和事件。' );
+localStorage.setItem( '5_HTML 5 音频', 'HTML5 规定了一种通过 audio 元素来包含音频的标准方法。audio 元素能够播放声音文件或者音频流。' );
+localStorage.setItem( '5_HTML 5 拖放', '拖放是一种常见的特性，即抓取对象以后拖到另一个位置。' );
+localStorage.setItem( '5_HTML 5 Canvas', 'canvas 元素用于在网页上绘制图形。' );
+localStorage.setItem( '5_HTML5 内联 SVG', 'SVG 指可伸缩矢量图形 (Scalable Vector Graphics)SVG 用于定义用于网络的基于矢量的图形SVG 使用 XML 格式定义图形SVG 图像在放大或改变尺寸的情况下其图形质量不会有损失SVG 是万维网联盟的标准' );
+localStorage.setItem( '5_HTML 5 Canvas vs. SVG', 'Canvas 和 SVG 都允许您在浏览器中创建图形，但是它们在根本上是不同的。' );
+localStorage.setItem( '5_HTML5 地理定位', 'HTML5 Geolocation（地理定位）用于定位用户的位置。' );
+localStorage.setItem( '5_HTML 5 Web 存储', '在客户端存储数据HTML5 提供了两种在客户端存储数据的新方法: localStorage - 没有时间限制的数据存储sessionStorage - 针对一个 session 的数据存储' );
+localStorage.setItem( '5_HTML 5 应用程序缓存', '使用 HTML5，通过创建 cache manifest 文件，可以轻松地创建 web 应用的离线版本。' );
+localStorage.setItem( '5_HTML 5 Web Workers', 'web worker 是运行在后台的 JavaScript，不会影响页面的性能。' );
+localStorage.setItem( '5_HTML 5 服务器发送事件', 'HTML5 服务器发送事件（server-sent event）允许网页获得来自服务器的更新。' );
+localStorage.setItem( '5_HTML5 Input 类型', 'email,url,number,range,Date,pickers (date, month, week, time, datetime, datetime-local), search, color' );
+localStorage.setItem( '5_HTML5 表单元素', 'datalist,keygen,output' );
+localStorage.setItem( '5_HTML5 表单属性', '新的 form 属性：autocomplete,novalidate。新的 input 属性：autocomplete,autofocus,form,form overrides (formaction, formenctype, formmethod, formnovalidate, formtarget),height 和 width,list,min, max 和 step,,multiple,pattern (regexp),placeholder,required' );
+localStorage.setItem( '5_HTML5 浏览器支持 - 音频', 'Firefox 支持 Ogg Vorbis 和 WAV, Opera 支持 Ogg Vorbis 和 WAV, Safari 支持 MP3、AAC 和 MP4, Chrome 支持 Ogg Vorbis、MP3、WAV, AAC 和 MP4, Internet Explorer 9+ 支持 MP3、AAC 和 MP4,iOS 支持 MP3、AAC 和 MP4, Android 支持 AAC 和 MP3' );
+localStorage.setItem( '5_HTML5 浏览器支持 - 视频', 'Firefox 支持 Theora Ogg 和 WebM,Opera 支持 Theora Ogg 和 WebM,Safari 支持 MP4,Chrome 支持 Theora Ogg、MP4 和 WebM,Internet Explorer 9+ 支持 MP4 和 WebM （需要插件）,iOS 支持 MP4,Android 支持 MP4 和 WebM (2.3+)' );
+localStorage.setItem( '5_HTML5 MIME 类型', '类似地添加视频支持：AddType video/webm webm AddType video/mp4 mp4 AddType video/ogg ogg AddType video/ogg ogv'+'<br />'+ '如果你没有权限访问服务器的 .htaccess 文件，那么你的控制板应该在某个地方能够允许你查看并添加 MIME 类型。');
+localStorage.setItem( '5_HTML5 文件转换', '很可能你的客户给你发了一个媒体文件，你也从文件的后缀判断出它的 MIME 类型。不过，文件的编码不一定是正确的。比如，它可能是一个没有问题的 MP4 文件，但不知什么原因，有的浏览器就是不能正确播放。如果这种情况发生，你最好自己用像 Miro Video Converter 或 Media Converter 这样的工具来检查或重新编码以确保编码的正确。此外，有些文件（特别是 MP4 文件）并不能总是正确播放，即使是支持该播放类型的浏览器。这是因为 MP4 （或是人们所知的 H.264）可以根据不同的 “profiles” 即配置文件来进行编码。如果有问题的文件是基于该浏览器不支持的 profile 进行编码的，很自然它不会正确播放。如果这种情况发生，尝试检查它是否是用 Baseline profile 进行编码的。采用 Baseline profile 比其它更先进的 profile 更容易得到支持。根据我的经验，使用像 Miro 一类的工具可以保证 MP4 文件可以在支持它的浏览器下正常工作。如果你需要支持 Firefox 3.6 或更低的版本，你需要把你的音频文件转换为 Ogg 格式，并给 <audio> 元素添加额外的 <source> 源。' );
+localStorage.setItem( '5_HTML5 MP4 文件在完全下载完后才开始播放', '通常 HTML5 的音视频播放器会允许媒体文件在完全下载之前就开始播放（只要有东西可放）。但有时候 MP4 文件只有等到整个文件下载完成才播放。这还是因为编码的问题。有时候 MP4 文件在编码过程中将文件的索引（它包含了如时长等信息）放到了媒体文件的尾部而不是头部。索引中包含了浏览器进行渐进式播放所需要的元数据。如果索引信息在文件的尾部，浏览器在获得索引之前便无从处理。如果你碰到这种情况，有个简单的处理方法，下载一个叫做 QTIndexSwapper 的工具，它可以把文件中的索引移动到文件头部并保存。' );
+localStorage.setItem( '5_HTML5 正确设置属性', '这看起来是显而易见的，但奇怪的是，我在 Stack Overflow 上还是看到人们多次问到为什么当他们用一些（不再使用的）属性时，有些功能不起作用。一个典型的例子是给 <audio> 和 <video> 元素应用 autobuffer 属性，而这个属性早在从 2010 年 2 月起便被 preload 替代了。人们看起来忘记了 HTML5 还没有最终定稿（当然大部分都已经相当稳定了），你读到的内容也许已经过时。最好是查一下 W3C HTML5 specification 以及 When Can I Use?（我什么时候可以用） 来随时了解浏览器的支持情况。' );
+localStorage.setItem( '5_HTML5 Firefox 11+ 的音量控制 ', '这是最近比较突出的一个问题。你可能已经注意到在 Firefox 11+ 下，音频播放器的音量控制消失了。当然，你可以设置静音，也可以用键盘的上下键来控制音量大小，但是音量控制的滑动条因为两个缺陷被移除了。我想它将来肯定会回来，但现在，万一你好奇想知道是怎么回事的时候，你好知道这不是你的错。当然，你也可以用 Media API 来制作你自己的控制部件。' );
+localStorage.setItem( '5_HTML5 需要插件 ', 'HTML5 在音频和视频方面的最大卖点之一便是你不需要任何第三方插件 （如 Flash）便可以播放媒体文件，只要浏览器支持 HTML5。不幸的是，在严格意义上，这并不正确。因为 Internet Explorer (9+) 和 Safari 分别需要系统中安装了 Microsoft Media Player 和 Apple QuickTime 才能正常播放 HTML5 的音频和视频文件。' );
+localStorage.setItem( '5_HTML5 视频和全屏显示 ', '视频播放中，最常见的需求便是全屏播放，但在 HTML5 的功能特性中并未涉及到这一点。不过，一个独立的 Fullscreen API 正在讨论中，一部分主流浏览器会有实验性版本可用。下列浏览器在一定程度上支持全屏 API，但要用到各自厂商的专用版本：Chrome 19+, Firefox 12+, Safari 5.1+, 还有一个 screenfull.js JavaScript polyfill 可以为一小部分浏览器提供全屏支持功能。' );
+localStorage.setItem( '5_HTML5 Internet Explorer 9+ 忽略了预览图', '如果你的 HTML5 视频（用 poster 属性）定义了预览图 ( poster image)，你可能会奇怪 IE 9 会完全忽略它，除非你把 preload 的属性设置为 none。IE 是最后一批支持 HTML5 的浏览器。我们已经适应了其它浏览器对于 poster 属性的处理：如果它已经定义，那么在用户开始播放视频之前会显示该图像。但 IE 9 并不是这么做的。它只有在完全没有内容可播放的情况下才会显示 poster 属性中包含的图像，所以当视频的第一帧被加载后（如果 preload 设置为默认值 auto 或 metadata），它就完全不理会 poster 里面的内容了。这看来是 IE 在按自己的方式进行处理，和其它的浏览器不一致。IE 10 目前也有同样的现象。' );
+localStorage.setItem( '5_HTML5 获取摄像头和麦克风 ', '当HTML5 特性的作者们在以前添加 <device> 元素时，获取像摄像头和麦克风这类周边设备的功能便一直是在考虑之中的。但后来它被移除，代之以更全面的 getUserMedia API，后者允许获取各种媒体设备。API 本身是容易使用的，但支持的浏览器目前非常有限。Opera 是现在唯一开始实施、且仅支持视频的浏览器，不过你已经可以看到它能做到什么了' );
+localStorage.setItem( '5_HTML5 采用HTML5开发是否意味着不必通过App Store的审核？', '非native的应用，即HTML5应用，由于可通过手机浏览器访问，所以是否就不必提交App Store并通过审核了？' );
+
+localStorage.setItem( '6_PHP 简介', 'PHP 文件可包含文本、HTML 标签以及脚本。PHP 文件中的脚本可在服务器上执行。' );
+localStorage.setItem( '6_什么是 PHP？', 'PHP 指 PHP:超文本预处理器（译者注：PHP: Hypertext Preprocessor，递归命名）,PHP 是一种服务器端的脚本语言，类似 ASP, PHP 脚本在服务器上执行, PHP 支持很多数据库（MySQL、Informix、Oracle、Sybase、Solid、PostgreSQL、Generic ODBC 等等）, PHP 是一个开源的软件（open source software，OSS）' );
+localStorage.setItem( '6_什么是 PHP 文件？', 'PHP 文件可包含文本、HTML 标签以及脚本,PHP 文件向浏览器返回纯粹的 HTML,PHP 文件的文件后缀是 ".php"、".php3" 或 ".phtml"' );
+localStorage.setItem( '6_什么是 MySQL？', 'MySQL 是一种数据库服务器，MySQL 支持标准的 SQL，MySQL 可在许多平台上编译，MySQL 可免费下载使用' );
+localStorage.setItem( '6_PHP + MySQL', 'PHP 与 MySQL 的组合是跨平台的（意思是您可以在 Windows 环境进行开发，而在 Unix 平台上提供服务）' );
+localStorage.setItem( '6_为什么要使用 PHP？', 'PHP 可在不同的平台上运行（Windows、Linux、Unix），PHP 与目前几乎所有的正在被使用的服务器相兼容（Apache、IIS 等），PHP 可从官方的 PHP 资源免费下载：www.php.net，PHP 易于学习，并可高效地运行在服务器端' );
+localStorage.setItem( '6_从何处开始？', '在 Windows 或者 Linux 机器上安装 Apache 服务器，在 Windows 或者 Linux 机器上安装 PHP，在 Windows 或者 Linux 机器上安装 MySQL' );
+localStorage.setItem( '6_PHP 语法', '您无法在浏览器中通过查看源文档的方式来查看 PHP 的源代码 - 您只能看到 PHP 文件的输出，即纯粹的 HTML。这是因为在结果返回浏览器之前，脚本就已经在服务器执行了。' );
+localStorage.setItem( '6_基本的 PHP 语法', 'PHP 的脚本块以 <?php 开始，以 ?> 结束。您可以把 PHP 的脚本块放置在文档中的任何位置。当然，在支持简写的服务器上，您可以使用 <? 和 ?> 来开始和结束脚本块。不过，为了达到最好的兼容性，我们推荐您使用标准形式 (<?php)，而不是简写形式。' );
+localStorage.setItem( '6_PHP 中的注释', '在 PHP 中，我们使用 // 来编写单行注释，或者使用 /* 和 */ 来编写大的注释块。' );
+localStorage.setItem( '6_PHP 变量', '变量用于存储值，比如数字、字符串或函数的结果，这样我们就可以在脚本中多次使用它们了。变量用于存储值，比如数字、文本字符串或数组。一旦设置了某个变量，我们就可以在脚本中重复地使用它。PHP 中的所有变量都是以 $ 符号开始的。' );
+localStorage.setItem( '6_PHP 是一门松散类型的语言', '在 PHP 中，不需要在设置变量之前声明该变量。' );
+localStorage.setItem( '6_变量的命名规则', '变量名必须以字母或下划线 "_" 开头。变量名只能包含字母数字字符以及下划线。变量名不能包含空格。如果变量名由多个单词组成，那么应该使用下划线进行分隔（比如 $my_string），或者以大写字母开头（比如 $myString）。' );
+localStorage.setItem( '6_PHP 字符串', '字符串变量用于存储并处理文本片段。' );
+localStorage.setItem( '6_PHP并置运算符', '在 PHP 中，只有一个字符串运算符。并置运算符 (.) 用于把两个字符串值连接起来。' );
+localStorage.setItem( '6_使用 strlen() 函数', 'strlen() 函数用于计算字符串的长度。' );
+localStorage.setItem( '6_使用 strpos() 函数', 'strpos() 函数用于在字符串内检索一段字符串或一个字符。' );
+localStorage.setItem( '6_PHP算数运算符', '+, -, *, /, %, ++, --' );
+localStorage.setItem( '6_PHP赋值运算符', '=, +=, -=, *=, /=, .=, %=' );
+localStorage.setItem( '6_PHP比较运算符', '=, !=, >, <, >=, <=' );
+localStorage.setItem( '6_PHP逻辑运算符', '&&, ||, !' );
+localStorage.setItem( '6_PHP If...Else 语句', 'if、elseif 以及 else 语句用于执行基于不同条件的不同动作。如果您希望在某个条件成立时执行一些代码，在条件不成立时执行另一些代码，请使用 if....else 语句。' );
+localStorage.setItem( '6_PHP ElseIf 语句', '如果希望在多个条件之一成立时执行代码，请使用 elseif 语句' );
+localStorage.setItem( '6_PHP Switch 语句', 'PHP 中的 Switch 语句用于执行基于多个不同条件的不同动作。' );
+localStorage.setItem( '6_PHP 数组', '数组能够在单独的变量名中存储一个或多个值。' );
+localStorage.setItem( '6_PHP 循环', 'PHP 中的循环语句用于执行相同的代码块指定的次数。' );
+localStorage.setItem( '6_while', '只要指定的条件成立，则循环执行代码块' );
+localStorage.setItem( '6_do...while', '首先执行一次代码块，然后在指定的条件成立时重复这个循环' );
+localStorage.setItem( '6_for', '循环执行代码块指定的次数' );
+localStorage.setItem( '6_foreach', '根据数组中每个元素来循环代码块' );
+localStorage.setItem( '6_PHP 函数', '函数是一种可以在任何被需要的时候执行的代码块。所有的函数都使用关键词 "function()" 来开始, 命名函数 - 函数的名称应该提示出它的功能。函数名称以字母或下划线开头。添加 "{" - 开口的花括号之后的部分是函数的代码。插入函数代码添加一个 "}" - 函数通过关闭花括号来结束。' );
+localStorage.setItem( '6_PHP 表单和用户输入', 'PHP 的 $_GET 和 $_POST 用于检索表单中的值，比如用户输入。' );
+localStorage.setItem( '6_PHP $_GET', '$_GET 变量用于收集来自 method="get" 的表单中的值。' );
+localStorage.setItem( '6_$_GET 变量', '$_GET 变量是一个数组，内容是由 HTTP GET 方法发送的变量名称和值。' );
+localStorage.setItem( '6_$_REQUEST 变量', 'PHP 的 $_REQUEST 变量包含了 $_GET, $_POST 以及 $_COOKIE 的内容。PHP 的 $_REQUEST 变量可用来取得通过 GET 和 POST 方法发送的表单数据的结果。' );
+localStorage.setItem( '6_PHP $_POST', '$_POST 变量用于收集来自 method="post" 的表单中的值。' );
+localStorage.setItem( '6_$_POST 变量', '$_POST 变量是一个数组，内容是由 HTTP POST 方法发送的变量名称和值。' );
+localStorage.setItem( '6_为什么使用 $_POST？', '通过 HTTP POST 发送的变量不会显示在 URL 中。变量没有长度限制。' );
+localStorage.setItem( '6_PHP Date()', 'PHP 的 date() 函数用于格式化时间或日期。' );
+localStorage.setItem( '6_PHP Date() 函数', 'PHP Date() 函数可把时间戳格式化为可读性更好的日期和时间。' );
+localStorage.setItem( '6_PHP 日期 - 什么是时间戳？', '时间戳是自 1970 年 1 月 1 日（00:00:00 GMT）以来的秒数。它也被称为 Unix 时间戳（Unix Timestamp）。' );
+localStorage.setItem( '6_PHP Include 文件', '服务器端包含 (SSI) 用于创建可在多个页面重复使用的函数、页眉、页脚或元素。' );
+localStorage.setItem( '6_PHP 文件处理', 'fopen() 函数用于在 PHP 中打开文件。fclose() 函数用于关闭打开的文件。feof() 函数检测是否已达到文件的末端 (EOF)。fgets() 函数用于从文件中逐行读取文件。fgetc() 函数用于从文件逐字符地读取文件。' );
+localStorage.setItem( '6_PHP 文件上传', '通过使用 PHP 的全局数组 $_FILES，你可以从客户计算机向远程服务器上传文件。' );
+localStorage.setItem( '6_PHP Cookies', 'cookie 常用于识别用户。' );
+localStorage.setItem( '6_什么是 Cookie？', 'cookie 常用于识别用户。cookie 是服务器留在用户计算机中的小文件。每当相同的计算机通过浏览器请求页面时，它同时会发送 cookie。通过 PHP，您能够创建并取回 cookie 的值。' );
+localStorage.setItem( '6_如何创建 cookie？', 'setcookie() 函数用于设置 cookie。' );
+localStorage.setItem( '6_如何取回 Cookie 的值？', 'PHP 的 $_COOKIE 变量用于取回 cookie 的值。' );
+localStorage.setItem( '6_如何删除 cookie？', '当删除 cookie 时，您应当使过期日期变更为过去的时间点。' );
+localStorage.setItem( '6_如果浏览器不支持 cookie 该怎么办？', '如果您的应用程序涉及不支持 cookie 的浏览器，您就不得不采取其他方法在应用程序中从一张页面向另一张页面传递信息。一种方式是从表单传递数据。' );
+
+localStorage.setItem( '7_SQL 能做什么','SQL面向数据库执行查询，SQL可从数据库取回数据，SQL可在数据库中插入新的记录，SQL 可更新数据库中的数据，SQL可从数据库删除记录，SQL 可创建新数据库，SQL 可在数据库中创建新表，SQL 可在数据库中创建存储过程，SQL 可在数据库中创建视图，SQL 可以设置表、存储过程和视图的权限。</br>SQL 是一门 ANSI 的标准计算机语言，用来访问和操作数据库系统。SQL 语句用于取回和更新数据库中的数据。SQL 可与数据库程序协同工作，比如 MS Access、DB2、Informix、MS SQL Server、Oracle、Sybase 以及其他数据库系统。数据库包括：Oracle, Sybase, SQL Server, DB2, Access' );
+localStorage.setItem( '7_SQL 语法','由数据库表构成，一个数据库通常包含一个或多个表。每个表由一个名字标识（例如“客户”或者“订单”）。表包含带有数据的记录（行）。您需要在数据库上执行的大部分工作都由 SQL 语句完成。' );
+localStorage.setItem( '7_SQL 语法主要注意事项','重要事项，一定要记住，SQL对大小写不敏感！SQL语句后面的分号？某些数据库系统要求在每条 SQL 命令的末端使用分号。在我们的教程中不使用分号。分号是在数据库系统中分隔每条 SQL语句的标准方法，这样就可以在对服务器的相同请求中执行一条以上的语句。如果您使用的是 MS Access 和 SQL Server 2000，则不必在每条 SQL 语句之后使用分号，不过某些数据库软件要求必须使用分号。' );
+localStorage.setItem( '7_SQL DML 和 DDL','可以把 SQL 分为两个部分：数据操作语言 (DML) 和 数据定义语言(DDL)。SQL (结构化查询语言)是用于执行查询的语法。但是 SQL 语言也包含用于更新、插入和删除记录的语法。查询和更新指令构成了 SQL 的 DML 部分：SELECT - 从数据库表中获取数据 DELETE - 从数据库表中删除数据INSERT INTO - 向数据库表中插入数据SQL 的数据定义语言 (DDL部分使我们有能力创建或删除表格。我们也可以定义索引（键），规定表之间的链接，以及施加表间的约束。SQL 中最重要的 DDL 语句:CREATE DATABASE - 创建新数据库ALTER DATABASE - 修改数据库CREATE TABLE - 创建新表ALTER TABLE - 变更（改变）数据库表 DROP TABLE - 删除表 CREATE INDEX - 创建索引（搜索键）DROP INDEX - 删除索引' );
+localStorage.setItem( '7_SQL SELECT 语句','SELECT语句用于从表中选取数据。结果被存储在一个结果表中（称为结果集）。SQL SELECT 语法，SELECT 列名称 FROM 表名称，以及：SELECT * FROM 表名称，注释：SQL 语句对大小写不敏感。SELECT 等效于 select。SQL SELECT * 实例现在我们希望从 "Persons" 表中选取所有的列。星号（*）是选取所有列的快捷方式。' );
+localStorage.setItem( '7_SQL SELECT DISTINCT 语句','在表中，可能会包含重复值。这并不成问题，不过，有时您也许希望仅仅列出不同（distinct）的值。关键词DISTINCT用于返回唯一不同的值。语法：SELECTDISTINCT列名称 FROM 表名称如果要从 "Company" 列中选取所有的值，我们需要使用 SELECT 语句：SELECT Company FROM Orders' );
+localStorage.setItem( '7_SQL Top子句', 'TOP 子句用于规定要返回的记录的数目。' );
+localStorage.setItem( '7_SQL Like操作符', 'LIKE 操作符用于在 WHERE 子句中搜索列中的指定模式。' );
+localStorage.setItem( '7_SQL 通配符', '在搜索数据库中的数据时，SQL 通配符可以替代一个或多个字符。SQL 通配符必须与 LIKE 运算符一起使用。' );
+localStorage.setItem( '7_SQL In', 'IN 操作符允许我们在 WHERE 子句中规定多个值。' );
+localStorage.setItem( '7_SQL Between', '操作符 BETWEEN ... AND 会选取介于两个值之间的数据范围。这些值可以是数值、文本或者日期。' );
+localStorage.setItem( '7_SQL Aliases', '通过使用 SQL，可以为列名称和表名称指定别名（Alias）。' );
+localStorage.setItem( '7_SQL Join', 'SQL join 用于根据两个或多个表中的列之间的关系，从这些表中查询数据。有时为了得到完整的结果，我们需要从两个或更多的表中获取结果。我们就需要执行 join。数据库中的表可通过键将彼此联系起来。主键（Primary Key）是一个列，在这个列中的每一行的值都是唯一的。在表中，每个主键的值都是唯一的。这样做的目的是在不重复每个表中的所有数据的情况下，把表间的数据交叉捆绑在一起。' );
+localStorage.setItem( '7_SQL Inner Join', '在表中存在至少一个匹配时，INNER JOIN 关键字返回行。' );
+localStorage.setItem( '7_SQL Left Join', 'LEFT JOIN 关键字会从左表 (table_name1) 那里返回所有的行，即使在右表 (table_name2) 中没有匹配的行。' );
+localStorage.setItem( '7_SQL Right Join', 'RIGHT JOIN 关键字会右表 (table_name2) 那里返回所有的行，即使在左表 (table_name1) 中没有匹配的行。' );
+localStorage.setItem( '7_SQL Full Join', '只要其中某个表存在匹配，FULL JOIN 关键字就会返回行。' );
+localStorage.setItem( '7_SQL Union', 'UNION 操作符用于合并两个或多个 SELECT 语句的结果集。请注意，UNION 内部的 SELECT 语句必须拥有相同数量的列。列也必须拥有相似的数据类型。同时，每条 SELECT 语句中的列的顺序必须相同。' );
+localStorage.setItem( '7_SQL Select Into', 'SELECT INTO 语句从一个表中选取数据，然后把数据插入另一个表中。SELECT INTO 语句常用于创建表的备份复件或者用于对记录进行存档。' );
+localStorage.setItem( '7_SQL Create OB', 'SQL CREATE DATABASE 用于创建数据库。语法：CREATE DATABASE database_name' );
+localStorage.setItem( '7_SQL Create Table', 'CREATE TABLE 语句用于创建数据库中的表。' );
+localStorage.setItem( '7_SQL 约束（Constraints）', '约束用于限制加入表的数据的类型。可以在创建表时规定约束（通过 CREATE TABLE 语句），或者在表创建之后也可以（通过 ALTER TABLE 语句）。' );
+localStorage.setItem( '7_SQL Not Null', 'NOT NULL 约束强制列不接受 NULL 值。NOT NULL 约束强制字段始终包含值。这意味着，如果不向字段添加值，就无法插入新记录或者更新记录。' );
+localStorage.setItem( '7_SQL Unique', 'UNIQUE 约束唯一标识数据库表中的每条记录。UNIQUE 和 PRIMARY KEY 约束均为列或列集合提供了唯一性的保证。PRIMARY KEY 拥有自动定义的 UNIQUE 约束。请注意，每个表可以有多个 UNIQUE 约束，但是每个表只能有一个 PRIMARY KEY 约束。' );
+localStorage.setItem( '7_SQL Primary Key', 'PRIMARY KEY 约束唯一标识数据库表中的每条记录。主键必须包含唯一的值。主键列不能包含 NULL 值。每个表都应该有一个主键，并且每个表只能有一个主键。' );
+localStorage.setItem( '7_SQL Foreign Key', '一个表中的 FOREIGN KEY 指向另一个表中的 PRIMARY KEY。' );
+localStorage.setItem( '7_SQL Check', 'CHECK 约束用于限制列中的值的范围。如果对单个列定义 CHECK 约束，那么该列只允许特定的值。如果对一个表定义 CHECK 约束，那么此约束会在特定的列中对值进行限制。' );
+localStorage.setItem( '7_SQL Default', 'DEFAULT 约束用于向列中插入默认值。如果没有规定其他的值，那么会将默认值添加到所有的新记录。' );
+localStorage.setItem( '7_SQL CREATE INDEX', 'CREATE INDEX 语句用于在表中创建索引。在不读取整个表的情况下，索引使数据库应用程序可以更快地查找数据。' );
+localStorage.setItem( '7_SQL Drop', '通过使用 DROP 语句，可以轻松地删除索引、表和数据库。' );
+localStorage.setItem( '7_SQL Alter', 'ALTER TABLE 语句用于在已有的表中添加、修改或删除列。' );
+localStorage.setItem( '7_SQL Increment', 'Auto-increment 会在新记录插入表中时生成一个唯一的数字。AUTO INCREMENT 字段我们通常希望在每次插入新记录时，自动地创建主键字段的值。我们可以在表中创建一个 auto-increment 字段。' );
+localStorage.setItem( '7_SQL View（视图）', '视图是可视化的表。' );
+localStorage.setItem( '7_SQL Date（函数）', 'SQL 日期当我们处理日期时，最难的任务恐怕是确保所插入的日期的格式，与数据库中日期列的格式相匹配。只要数据包含的只是日期部分，运行查询就不会出问题。但是，如果涉及时间，情况就有点复杂了。在讨论日期查询的复杂性之前，我们先来看看最重要的内建日期处理函数。' );
+localStorage.setItem( '7_SQL Null（值）', 'NULL 值是遗漏的未知数据。默认地，表的列可以存放 NULL 值。本章讲解 IS NULL 和 IS NOT NULL 操作符。' );
+localStorage.setItem( '7_SQL isnull()（函数）', 'SQL ISNULL()、NVL()、IFNULL() 和 COALESCE() 函数' );
+localStorage.setItem( '7_SQL 数据类型', 'Microsoft Access、MySQL 以及 SQL Server 所使用的数据类型和范围。' );
+localStorage.setItem( '7_SQL 服务器', '现代的 SQL 服务器构建在 RDBMS 之上。DBMS - 数据库管理系统（Database Management System）数据库管理系统是一种可以访问数据库中数据的计算机程序。DBMS 使我们有能力在数据库中提取、修改或者存贮信息。不同的 DBMS 提供不同的函数供查询、提交以及修改数据。' );
---
+++ baodian/js/zside.js
@@ -0,0 +1,247 @@
+﻿ /**
+ *	DESC:	A Simple Library Based On Zepto, For Mobile Device
+ *	AUTHOR:	1203A
+ *	SINCE:	2013-04-06
+ *	EMAIL:	gao_st@126.com
+ */
+;( function ( window, $ ) {
+	// NO_NEED_LOAD_DATA = [ '#main', '#config' ];
+	var DATA_MAPPING = [ 'v', 'css', 'css3', 'js', 'jquery', 'html5', 'php', 'sql', 'srsy', 'about' ];
+	var myScroll, searchScroll, historyScroll;
+	
+	$( document ).ready( function () {
+		bindAnchorEvt();
+		myScroll = new iScroll('list', { checkDOMChanges: true, fadeScrollbar: true });
+		searchScroll = new iScroll('search', { checkDOMChanges: true, fadeScrollbar: true });
+		historyScroll = new iScroll('history', { checkDOMChanges: true, fadeScrollbar: true });
+		bindSearchEvt();
+		bindFooterTapEvt();
+		initData();
+	});
+	
+	function bindAnchorEvt () {
+		$('#wrapper, #footer, #header').on( 'click', 'a[href]', function ( e ) {
+			var tmpId = $(this).attr('href'),
+				title = $(this).attr('title');
+			
+			$( tmpId ).css({
+				'-webkit-transition': '-webkit-transform 200ms',
+				'-webkit-transform-origin': '0% 0%', 
+				'-webkit-transform': 'translate(0%, 0%) scale(1) translateZ(0px)',
+				'opacity': 1
+			}).siblings().css({
+				'-webkit-transition': '-webkit-transform 200ms',
+				'-webkit-transform-origin': '0% 0%', 
+				'-webkit-transform': 'translate(100%, 0%) scale(1) translateZ(0px)',
+				'opacity': 0
+			});
+			
+			e.preventDefault();
+			
+			// console.log( tmpId )
+			reviseTopBar( title, tmpId );
+			
+			if ( tmpId == '#list' ) {
+				tmpId = $(this).attr('id');
+				loadDataById( tmpId );
+			} else if ( tmpId == '#detail' ) {
+				tmpId = $(this).attr('title');
+				showDetails( tmpId );
+				addToHistory( tmpId );
+			} else if ( tmpId == '#history' ) {
+				showHistory();
+			}
+			
+		});
+	}
+	
+	function loadDataById( id ) {
+		var index;
+		/* if ( $.inArray( id, NO_NEED_LOAD_DATA ) > -1 ) {
+			return;
+		} */
+		
+		index = $.inArray( id, DATA_MAPPING );
+		
+		if ( index > -1 ) {
+			getData( index );
+		}
+	}
+	
+	function getData( index ) {
+		var ls = window.localStorage,
+			length = ls.length, 
+			i,
+			tmpKey,
+			tmpArr = [];
+		
+		for ( i=0; i<length; i++ ) {
+			tmpKey = ls.key(i);
+			if ( tmpKey.indexOf( index ) == 0 ) {
+				tmpArr.push( reviseKey( tmpKey ) );				
+			}
+		}
+		renderUL( tmpArr, index );
+	}
+	
+	function renderUL ( arr, index ) {
+		var str = '', i, length;
+		if ( arr ) {
+			length = arr.length;
+			for ( i=0; i<length; i++ ) {
+				str += '<li><a href="#detail" title="'+index+'_'+arr[i]+'">' + arr[i] + '</a></li>';
+			}
+		
+			// str = '<li><a href="#detail" id="123">' + arr.join('</a></li><li><a href="#detail" id="123">') + '</a></li>';
+		}
+		$('#list ul').html( str );
+		
+		myScroll.scrollTo( 0, 0, 100 );
+	}
+	
+	function showDetails( key ) {
+		var val = _getValueByKey( key );
+		$('#detail').html( '<p>'+val+'</p>' );
+	}
+	
+	function reviseTopBar( title, target ) {
+		
+		var $leftBtn = $( '#leftBtn' ),
+			$rightBtn = $( '#rightBtn' ),
+			$header = $('#header'); 
+		
+		if ( title ) {
+			$('#header label').text( reviseKey( title ) );
+		} else {
+			$('#header label').text( '面试宝典' );
+		}
+		
+		if ( target == '#list' ) {
+			$leftBtn.show().attr('href', '#main').removeAttr('title');
+			sessionStorage.setItem( 'tmpTitle', title );
+		} else if ( target == '#detail' ) {
+			$leftBtn.show().attr('href', '#list').attr('title', sessionStorage.getItem('tmpTitle') );
+		} else if ( target == '#search' ) {
+			// $leftBtn.show();
+			$header.hide();
+		} else {
+			$leftBtn.hide();
+			$header.show();
+			// $rightBtn.show();
+		}
+	}
+	
+	function reviseKey( key ) {
+		return key.substring( key.indexOf('_') + 1 );
+	}
+	
+	function bindSearchEvt() {
+		var $searchDiv = $('#searchDiv'),
+			$searchTxt = $('#searchTxt'),
+			val;
+		$searchDiv.on( 'click', function () {
+			val = $.trim( $searchTxt.val() );
+			if ( val != '' ) {
+				getSearchResults( val );
+			}
+		});
+	}
+	
+	function getSearchResults( txt ) {
+		var ls = window.localStorage,
+			length = ls.length, 
+			i,
+			tmpKey,
+			tmpArr = [];
+		
+		for ( i=0; i<length; i++ ) {
+			tmpKey = ls.key(i);
+			if ( tmpKey.toLowerCase().indexOf( txt.toLowerCase() ) > -1 ) {
+				tmpArr.push( tmpKey );				
+			}
+		}
+		
+		renderSearchUL( tmpArr );
+	}
+	
+	function renderSearchUL ( arr ) {
+		var str = '', i, length;
+		if ( arr ) {
+			length = arr.length;
+			for ( i=0; i<length; i++ ) {
+				str += '<li><a href="#detail" title="'+arr[i]+'">' + reviseKey( arr[i] ) + '</a></li>';
+			}
+		
+		}
+		$('#search ul').html( str );
+		
+		// searchScroll.refresh();
+		// myScroll.scrollTo( 0, 0, 100 );
+	}
+	
+	function bindFooterTapEvt () {
+		var $masker = $( '#footer .masker'),
+			index;
+		$('#footer a').on( 'click', function () {
+			index = $(this).index() * 100;
+			$masker.css({
+				'-webkit-transition': '-webkit-transform 200ms',
+				'-webkit-transform-origin': '0% 0%', 
+				'-webkit-transform': 'translate('+index+'%, 0%) scale(1) translateZ(0px)'
+			});
+		});
+	}
+	
+	function initData () {
+		var baseUrl = 'http://localhost/baodian/data/';
+		var ls = window.localStorage, val;
+		if ( ls ) {
+			val = ls.getItem( 'v' );
+		}
+
+		$.get( baseUrl + 'version.json?t='+new Date().getTime(), function ( data ) {
+			if ( !val || data.v > val ) { // &&
+				console.log( 'init data...' );
+				$.ajax({
+					url: baseUrl + 'data.js?t='+new Date().getTime(),
+					dataType: 'jsonp'
+				});
+			}
+		});
+	}
+	
+	function addToHistory ( key ) {
+		var h = _getValueByKey( 'h' ), hObj = JSON.parse( h );
+		hObj.push( key );
+		_setValueByKey( 'h', JSON.stringify( hObj ) );
+		// console.log( hObj );
+	}
+	
+	function showHistory () {
+		var arr = _getValueArrayByKey( 'h' ).reverse(), 
+			i, len = arr.length, str = '';
+		for ( i=0; i<len; i++ ) {
+			str += '<li><a href="#detail" title="'+arr[i]+'">' + reviseKey( arr[i] ) + '</a></li>';
+		}
+		$('#history ul').html( str );
+	}
+	
+	function _getValueByKey ( key ) {
+		var ls = window.localStorage, val;
+		if ( key ) {
+			val = ls.getItem( key );
+			return val;
+		}
+	}
+	
+	function _setValueByKey ( key, val ) {
+		var ls = window.localStorage;
+		ls.setItem( key, val );
+	}
+	
+	function _getValueArrayByKey ( key ) {
+		var ls = window.localStorage, arr;
+		arr = JSON.parse( ls.getItem( key ) );
+		return arr;
+	}
+})( window, Zepto );
